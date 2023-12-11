6df767e99ab7dcea0e91a885a4254c461070847d
johamatt94 <johamatt94@gmail.com>
2023-12-11 12:33:16 +0200

    tehtävä 2.20*

--- osa2/maiden-tiedot/src/Components/Content.tsx
+++ osa2/maiden-tiedot/src/Components/Content.tsx
@@ -5,21 +5,50 @@ interface ContentProps {
   results: Array<any>;
   search: string;
 }
-
 const api_key = import.meta.env.VITE_SOME_KEY;
-// muuttujassa api_key on nyt käynnistyksessä annettu API-avaimen arvo
+
+console.log(api_key);
+// ($env:VITE_SOME_KEY="") -and (npm run dev) // Windows PowerShell
 
 export const Content: React.FC<ContentProps> = ({ results, search }) => {
   const [countries, setCountries] = useState<Array<any>>([]);
 
   const [showCountry, setShowCountry] = useState<any>();
+  const [showCountryWeather, setShowCountryWeather] = useState<any>();
+
+  const [a, seta] = useState<any>();
 
   useEffect(() => {
     searchMatches();
     setShowCountry(undefined);
   }, [results]);
 
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const result = await axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${showCountry.capitalInfo.latlng[0]}&lon=${showCountry.capitalInfo.latlng[1]}&units=metric&appid=${api_key}`
+          )
+          .then((result) => result.data);
+
+        console.log(result);
+        setShowCountryWeather(result);
+      } catch (e) {
+        console.error("Error fetching weather:", e);
+      }
+    };
+
+    fetchWeather(); // Call the function immediately
+  }, [showCountry]);
+
+  console.log(a);
+
+  console.log(showCountryWeather);
+  console.log(showCountry);
+
   const showOneCountry = (country: any) => {
+    console.log(showCountryWeather);
     return (
       <div>
         <h1>{country.name.common}</h1>
@@ -31,7 +60,16 @@ export const Content: React.FC<ContentProps> = ({ results, search }) => {
             return <li key={i}>{name}</li>;
           })}
         </ul>
-        <img src={country.flags.png} />
+        <img src={country.flags.png} alt="Country Flag" />
+
+        <div>
+          <h2>Weather in Helsinki</h2>
+          <h3>Temperature {showCountryWeather?.main.temp} Celcius</h3>
+          <img
+            src={`https://openweathermap.org/img/wn/${showCountryWeather?.weather[0].icon}@2x.png`}
+          />
+          <p>Wind: {showCountryWeather?.wind.speed}</p>
+        </div>
       </div>
     );
   };
@@ -41,27 +79,20 @@ export const Content: React.FC<ContentProps> = ({ results, search }) => {
       return a.name.common.toLowerCase().includes(search.toLowerCase());
     });
     setCountries(matchingCountries);
-    console.log(countries);
-  };
-
-  const fetchIcon = async (country: any) => {
-    const result = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat={${country.lat}}&lon={${country.lon}}&appid={${api_key}}`
-    );
-    console.log(result);
   };
 
   if (countries.length > 10) {
     return <p>Too many Matches, specify another filter</p>;
   } else if (countries.length === 1) {
+    setShowCountry(countries[0]);
     return showOneCountry(countries[0]);
   } else if (countries.length < 10 && countries.length !== 1) {
     return (
       <>
         <ul>
-          {countries.map((a) => {
+          {countries.map((a, i) => {
             return (
-              <li>
+              <li key={i}>
                 {a.name.common}
                 <button onClick={() => setShowCountry(a)}>Show</button>
               </li>
@@ -69,7 +100,6 @@ export const Content: React.FC<ContentProps> = ({ results, search }) => {
           })}
         </ul>
         {showCountry ? showOneCountry(showCountry) : <div />}
-        {showCountry ? fetchIcon(showCountry) : <div />}
       </>
     );
   } else {
