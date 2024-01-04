f9de8328ddb19003f66a7d2f41562ac0a8c5f83d
johamatt94 <johamatt94@gmail.com>
2024-01-04 14:45:40 +0200

    poistettu ylimääräinen listener

--- osa3/puhelinluettelo-backend/src/index.ts
+++ osa3/puhelinluettelo-backend/src/index.ts
@@ -5,9 +5,6 @@ const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 app.use(cors());
 app.use(express.json());
